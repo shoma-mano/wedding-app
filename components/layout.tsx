e70c2c6190954c505e9e93f1f0b54e0e918f3a45
manowaraiuma <mano@r-unit.co.jp>
2022-02-12 02:11:58 +0900

    完成

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,110 +1,146 @@
-import { Box, Image, Text } from '@chakra-ui/react';
+import { Box, Image, Text, useBreakpoint, useBreakpointValue } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
+import { useEffect, useState } from "react";
 
-export const Layout = ({ children }: { children: React.ReactElement }) => {
-  const menus = ['HOME', 'MESSAGE', 'PHOTOS'];
-  const router = useRouter();
-  const movePage = (page: string) => {
-    if (page === 'HOME') router.push('/', undefined, { scroll: false });
-    else router.push(`/${page.toLowerCase()}`, undefined, { scroll: false });
-  };
+export const Layout = ({children}: { children: React.ReactElement }) => {
+    const menus = ['MESSAGE', 'PHOTOS'];
+    const router = useRouter();
+    const date =new Date('2022-03-05');
+    const nowDate = new Date();
+    const day = date-nowDate
+    const weddingDay=parseInt(String(day / 1000 / 60 / 60 / 24))
+    const [mounted, setMounted] = useState(false)
+    const movePage = (page: string) => {
+        if (page === 'HOME') router.push('/', undefined, {scroll: false});
+        else router.push(`/${page.toLowerCase()}`, undefined, {scroll: false});
+    };
 
-  return (
-    <>
-      <Head>
-        <title>Image Uploader</title>
-        <meta name='description' content='Upload your image to Cloudinary!' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <Box
-        minHeight={'100vh'}
-        display={'flex'}
-        flexDirection={'column'}
-        alignItems={'center'}
-        backgroundColor={'#FFFAF1'}
-      >
-        <Image
-          src={
-            'https://media-api.xogrp.com/images/3f93e2fc-fe94-4940-9bfe-7923d13e6a5c~rt_auto-rs_1024.h?ordering=explicit'
-          }
-        />
-        <Text mt={'25px'} fontSize={'63px'} fontFamily={"'Euphoria Script', cursive;"}>
-          Satoko & Masaki
-        </Text>
-        <Image width={'60%'} borderRadius={'1px'} src={'/s1_img1.png'} />
-        <Text mt={'15px'} fontSize={'28px'} fontFamily={"'Cutive Mono', monospace;"}>
-          March 5,2022 Tokyo Suginami Church
-        </Text>
-        <Text mt={'5px'} fontSize={'30px'} fontFamily={"'Cutive Mono', monospace;"}>
-          30 Days To Go
-        </Text>
-        <Box mt={'50px'} display={'flex'} width={'60%'} justifyContent={'space-between'}>
-          {menus.map((v) => (
+
+    const breakpoint = useBreakpointValue({
+            base: (<>
+                    <Text mt={'15px'} fontSize={'20px'} fontFamily={"'Cutive Mono', monospace;"}>
+                        March 5,2022
+                    </Text>
+                    <Text mt={'15px'} style={{fontSize: '20px'}} fontSize={'20px !important'}
+                          fontFamily={"'Cutive Mono', monospace;"}>
+                        Tokyo Suginami Church
+                    </Text>
+                </>
+            ),
+            sm: (
+                <>
+                    <Text mt={'15px'} fontSize={'20px'} fontFamily={"'Cutive Mono', monospace;"}>
+                        March 5,2022
+                    </Text>
+                    <Text mt={'15px'} style={{fontSize: '20px'}} fontSize={'20px !important'}
+                          fontFamily={"'Cutive Mono', monospace;"}>
+                        Tokyo Suginami Church
+                    </Text>
+                </>
+            ),
+            md: (<Text mt={'15px'} fontSize={'28px'} fontFamily={"'Cutive Mono', monospace;"}>
+                March 5,2022 Tokyo Suginami Church
+            </Text>)
+        }
+    )
+
+    useEffect(()=>{
+        setMounted(true)
+    },[])
+
+    return (
+        <>
+            <Head>
+                <title>Image Uploader</title>
+                <meta name='description' content='Upload your image to Cloudinary!'/>
+                <link rel='icon' href='/favicon.ico'/>
+            </Head>
             <Box
-              onClick={() => movePage(v)}
-              cursor={'pointer'}
-              key={v}
-              height={'30px'}
-              width={'80px'}
-              boxSizing={'border-box'}
+                minHeight={'100vh'}
+                display={'flex'}
+                flexDirection={'column'}
+                alignItems={'center'}
+                backgroundColor={'#FFFAF1'}
             >
-              <Text
-                fontSize={'25px'}
-                display={'inline-block'}
-                fontFamily={"'Shadows Into Light', cursive;"}
-                _hover={{ borderBottom: '2px solid black' }}
-                transition={'border 0.5s, border-width 0.01s'}
-              >
-                {v}
-              </Text>
-              <Box width={'100%'} />
-            </Box>
-          ))}
-        </Box>
-        <Box
-          width={'70%'}
-          pt={'70px'}
-          display={'flex'}
-          flexDirection={'column'}
-          alignItems={'center'}
-        >
-          {children}
-        </Box>
-        <Text mt={'105px'} fontSize={'40px'} fontFamily={"'Shadows Into Light', cursive;"}>
-          Wedding Day
-        </Text>
-        <Text mt={'5px'} fontFamily={"'Cutive Mono', monospace;"} fontSize={'25px'}>
-          MARCH 5,2022
-        </Text>
-        <Text
-          color={'#3C2014'}
-          width={'160px'}
-          textAlign={'center'}
-          borderBottom={'solid 1px'}
-          mt={'90px'}
-          fontFamily={"'Euphoria Script', cursive;"}
-          fontSize={'55px'}
-        >
-          S&M
-        </Text>
-        <Text
-          color={'#3C2014'}
-          mt={'10px'}
-          fontFamily={"'Cutive Mono', monospace;"}
-          fontSize={'20px'}
-          fontWeight={'bold'}
-        >
-          2022.3.5
-        </Text>
+                <Image
+                    src={
+                        'https://media-api.xogrp.com/images/3f93e2fc-fe94-4940-9bfe-7923d13e6a5c~rt_auto-rs_1024.h?ordering=explicit'
+                    }
+                />
+                <Text mt={'25px'} fontSize={'63px'} fontFamily={"'Euphoria Script', cursive;"}>
+                    Satoko & Masaki
+                </Text>
+                <Image width={'60%'} borderRadius={'1px'} src={'/main.png'}/>
+                {mounted && breakpoint}
+                <Text mt={'5px'} fontSize={'30px'} fontFamily={"'Cutive Mono', monospace;"}>
+                    {weddingDay} Days To Go
+                </Text>
+                <Box mt={'50px'} display={'flex'} width={'50%'} justifyContent={'space-evenly'}>
+                    {menus.map((v) => (
+                        <Box
+                            onClick={() => movePage(v)}
+                            cursor={'pointer'}
+                            key={v}
+                            height={'30px'}
+                            boxSizing={'border-box'}
+                        >
+                            <Text
+                                fontSize={['18px', '25px']}
+                                display={'inline-block'}
+                                fontFamily={"'Shadows Into Light', cursive;"}
+                                _hover={{borderBottom: '2px solid black'}}
+                                transition={'border 0.5s, border-width 0.01s'}
+                            >
+                                {v}
+                            </Text>
+                            <Box width={'100%'}/>
+                        </Box>
+                    ))}
+                </Box>
+                <Box
+                    width={'70%'}
+                    pt={'70px'}
+                    display={'flex'}
+                    flexDirection={'column'}
+                    alignItems={'center'}
+                >
+                    {children}
+                </Box>
+                <Text mt={'105px'} fontSize={'40px'} fontFamily={"'Shadows Into Light', cursive;"}>
+                    Wedding Day
+                </Text>
+                <Text mt={'5px'} fontFamily={"'Cutive Mono', monospace;"} fontSize={'25px'}>
+                    MARCH 5,2022
+                </Text>
+                <Text
+                    color={'#3C2014'}
+                    width={'160px'}
+                    textAlign={'center'}
+                    borderBottom={'solid 1px'}
+                    mt={'90px'}
+                    fontFamily={"'Euphoria Script', cursive;"}
+                    fontSize={'55px'}
+                >
+                    S&M
+                </Text>
+                <Text
+                    color={'#3C2014'}
+                    mt={'10px'}
+                    fontFamily={"'Cutive Mono', monospace;"}
+                    fontSize={'20px'}
+                    fontWeight={'bold'}
+                >
+                    2022.3.5
+                </Text>
 
-        <Image
-          mt={'80px'}
-          src={
-            'https://media-api.xogrp.com/images/f40efb40-4576-4cf3-9e82-58ead3c4ac55~rt_auto-rs_1536.h?ordering=explicit'
-          }
-        />
-      </Box>
-    </>
-  );
+                <Image
+                    mt={'80px'}
+                    src={
+                        'https://media-api.xogrp.com/images/f40efb40-4576-4cf3-9e82-58ead3c4ac55~rt_auto-rs_1536.h?ordering=explicit'
+                    }
+                />
+            </Box>
+        </>
+    );
 };
--- pages/photos/index.tsx
+++ pages/photos/index.tsx
@@ -1,172 +1,185 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
-import { Box, Image, Text, Icon,Stack } from '@chakra-ui/react';
+import { Box, Image, Text, Icon, Stack } from '@chakra-ui/react';
 import { useState } from 'react';
 import { CloseIcon } from '@chakra-ui/icons';
 import { gql, useMutation, useQuery } from '@apollo/client';
 
 const Photos: NextPage = () => {
-  const [imageSrc, setImageSrc] = useState<any>();
-  const [uploadData, setUploadData] = useState<any>();
+    const [imageSrc, setImageSrc] = useState<any>();
+    const [uploadData, setUploadData] = useState<any>();
 
-  const GET_URL = gql`
-    query get_url {
-      image {
-        url
-      }
-    }
-  `;
-
-  const SAVE_URL = gql`
-    mutation MyMutation($url: String!) {
-      insert_image(objects: { url: $url }) {
-        returning {
-          id
-          url
+    const GET_URL = gql`
+        query get_url {
+            image {
+                url
+            }
         }
-      }
-    }
-  `;
+    `;
 
-  const [saveUrl, { data, loading, error }] = useMutation(SAVE_URL, {
-    refetchQueries: [GET_URL, 'get_url'],
-  });
-  const { data: getData, loading: getLoading } =
-    useQuery<{ image: Array<{ url: string }> }>(GET_URL);
+    const SAVE_URL = gql`
+        mutation MyMutation($url: String!) {
+            insert_image(objects: { url: $url }) {
+                returning {
+                    id
+                    url
+                }
+            }
+        }
+    `;
 
-  //Triggers when the file input changes (ex: when a file is selected)
-  const handleOnChange = (changeEvent: any) => {
-    const reader = new FileReader();
+    const [saveUrl, {data, loading, error}] = useMutation(SAVE_URL, {
+        refetchQueries: [GET_URL, 'get_url'],
+    });
+    const {data: getData, loading: getLoading} =
+        useQuery<{ image: Array<{ url: string }> }>(GET_URL);
 
-    reader.onload = function (onLoadEvent) {
-      setImageSrc(onLoadEvent.target!.result);
-      setUploadData(undefined);
+    //Triggers when the file input changes (ex: when a file is selected)
+    const handleOnChange = (changeEvent: any) => {
+        const reader = new FileReader();
+
+        reader.onload = function (onLoadEvent) {
+            setImageSrc(onLoadEvent.target!.result);
+            setUploadData(undefined);
+        };
+
+        reader.readAsDataURL(changeEvent.target.files[0]);
     };
 
-    reader.readAsDataURL(changeEvent.target.files[0]);
-  };
+    const handleOnSubmit = async (event: any) => {
+        event.preventDefault();
+        const form = event.currentTarget;
+        const fileInput: any = Array.from(form.elements).find(({name}: any) => name === 'file');
 
-  const handleOnSubmit = async (event: any) => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const fileInput: any = Array.from(form.elements).find(({ name }: any) => name === 'file');
+        const formData = new FormData();
+        for (const file of fileInput.files) {
+            formData.append('file', file);
+        }
 
-    const formData = new FormData();
-    for (const file of fileInput.files) {
-      formData.append('file', file);
-    }
+        formData.append('upload_preset', 'ml_default');
 
-    formData.append('upload_preset', 'ml_default');
+        const data = await fetch('https://api.cloudinary.com/v1_1/dsu2ajyhb/upload', {
+            method: 'POST',
+            body: formData,
+        }).then((r) => r.json());
 
-    const data = await fetch('https://api.cloudinary.com/v1_1/dsu2ajyhb/upload', {
-      method: 'POST',
-      body: formData,
-    }).then((r) => r.json());
+        saveUrl({variables: {url: data.secure_url}}).then(() => setImageSrc(''));
+    };
 
-    saveUrl({ variables: { url: data.secure_url } }).then((r) => console.log(r));
-  };
-
-  return (
-    <Box display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'}>
-      <Stack gap={'50px'}>
-      {!getLoading && getData!.image.map((v) => <Image width={'350px'} borderRadius={'5px'} key={v.url} src={v.url}></Image>)}
-      </Stack>
-      <form
-        className={styles.form}
-        method='post'
-        onChange={handleOnChange}
-        onSubmit={handleOnSubmit}
-        style={{marginTop:'100px'}}
-      >
-        <Box
-          mx={'auto'}
-          role='group'
-          color={'#AAA'}
-          position={'relative'}
-          cursor={'pointer'}
-          borderRadius={'15px'}
-          py={imageSrc ? '20px' : '0px'}
-          width={imageSrc ? '' : '400px'}
-          height={imageSrc ? '' : '100px'}
-          backgroundColor={'#eee'}
-        >
-          {imageSrc && (
-            <CloseIcon
-              onClick={() => setImageSrc('')}
-              _hover={{ color: 'black' }}
-              position={'absolute'}
-              top={'15px'}
-              right={'15px'}
-              zIndex={110}
-            />
-          )}
-
-          {< input
-            style={{
-              cursor: 'pointer',
-              position: 'absolute',
-              zIndex: 100,
-              opacity: 0,
-              width: imageSrc? '0px' :'400px',
-              height: imageSrc?'0px':'100px',
-            }}
-            className={styles.fileinput}
-            type='file'
-            name='file'
-          />}
-          {!imageSrc && (
-            <Text
-              fontFamily={'monospace'}
-              fontSize={'25px'}
-              position={'relative'}
-              top={'30px'}
-              textAlign={'center'}
-              zIndex={'10'}
-              _groupHover={{ color: '#000000ab' }}
+    return (
+        <Box display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'}>
+            <Stack gap={'50px'}>
+                {!getLoading && getData!.image.map((v) =>
+                    <Image width={'350px'} borderRadius={'5px'} key={v.url}
+                           src={v.url}/>)}
+            </Stack>
+            <form
+                className={styles.form}
+                method='post'
+                onChange={handleOnChange}
+                onSubmit={handleOnSubmit}
+                style={{marginTop: '100px'}}
             >
-              みんなで写真を共有しよう
-              <Icon
-                xmlns='http://www.w3.org/2000/svg'
-                version='1.1'
-                ml={'8px'}
-                mb={'3px'}
-                fontSize={'20px'}
-                x='0px'
-                y='0px'
-                viewBox='0 0 1000 1000'
-                enableBackground='new 0 0 1000 1000'
-                _groupHover={{ color: '#000000ab' }}
-              >
-                <g>
-                  <path
-                    fill='currentColor'
-                    d='M850,974.5H150c-77.3,0-140-65.3-140-145.9V646.3c0-20.1,15.7-36.5,35-36.5h70c19.3,0,35,16.3,35,36.5v109.4c0,40.3,31.3,72.9,70,72.9h560c38.7,0,70-32.7,70-72.9V646.3c0-20.1,15.7-36.5,35-36.5h70c19.3,0,35,16.3,35,36.5v182.3C990,909.2,927.3,974.5,850,974.5L850,974.5z M784.5,449.2c-14.2,14.8-37.1,14.8-51.3,0L570,279.1v367.2c0,20.1-15.7,36.5-35,36.5h-70c-19.3,0-35-16.3-35-36.5V279.1L266.8,449.2c-14.2,14.8-37.1,14.8-51.3,0l-51.3-53.4c-14.2-14.8-14.2-38.7,0-53.4L453.2,41.1c1.2-1.3,23.7-15.6,46.4-15.6c22.9,0,45.9,14.3,47.2,15.6l289.1,301.2c14.2,14.8,14.2,38.7,0,53.4L784.5,449.2L784.5,449.2z'
-                  />
-                </g>
-              </Icon>
-            </Text>
-          )}
+                <Box
+                    mx={'auto'}
+                    role='group'
+                    color={'#AAA'}
+                    position={'relative'}
+                    cursor={'pointer'}
+                    borderRadius={'15px'}
+                    py={imageSrc ? '20px' : '0px'}
+                    width={imageSrc ? '' : '400px'}
+                    maxWidth={'80vw'}
+                    height={imageSrc ? '' : '100px'}
+                    backgroundColor={'#eee'}
+                >
+                    {imageSrc && (
+                        <CloseIcon
+                            onClick={() => setImageSrc('')}
+                            _hover={{color: 'black'}}
+                            position={'absolute'}
+                            top={'15px'}
+                            right={'15px'}
+                            zIndex={110}
+                        />
+                    )}
 
-          {imageSrc && (
-            <Image borderRadius={'5px'} my={'auto'} mx={'auto'} width={'50%'} maxWidth={'300px'} src={imageSrc} />
-          )}
+                    {<input
+                        style={{
+                            cursor: 'pointer',
+                            position: 'absolute',
+                            zIndex: 100,
+                            opacity: 0,
+                            width: imageSrc ? '0px' : '400px',
+                            maxWidth: '80vw',
+                            height: imageSrc ? '0px' : '100px',
+                        }}
+                        className={styles.fileinput}
+                        type='file'
+                        name='file'
+                    />}
+                    {!imageSrc && (
+                        <Text
+                            fontFamily={'monospace'}
+                            fontSize={['18px', '25px']}
+                            position={'relative'}
+                            top={['40px', '30px']}
+                            textAlign={'center'}
+                            zIndex={'10'}
+                            _groupHover={{color: '#000000ab'}}
+                        >
+                            みんなで写真を共有しよう
+                            <Icon
+                                xmlns='http://www.w3.org/2000/svg'
+                                version='1.1'
+                                ml={'8px'}
+                                mb={'3px'}
+                                fontSize={'20px'}
+                                x='0px'
+                                y='0px'
+                                viewBox='0 0 1000 1000'
+                                enableBackground='new 0 0 1000 1000'
+                                _groupHover={{color: '#000000ab'}}
+                            >
+                                <g>
+                                    <path
+                                        fill='currentColor'
+                                        d='M850,974.5H150c-77.3,0-140-65.3-140-145.9V646.3c0-20.1,15.7-36.5,35-36.5h70c19.3,0,35,16.3,35,36.5v109.4c0,40.3,31.3,72.9,70,72.9h560c38.7,0,70-32.7,70-72.9V646.3c0-20.1,15.7-36.5,35-36.5h70c19.3,0,35,16.3,35,36.5v182.3C990,909.2,927.3,974.5,850,974.5L850,974.5z M784.5,449.2c-14.2,14.8-37.1,14.8-51.3,0L570,279.1v367.2c0,20.1-15.7,36.5-35,36.5h-70c-19.3,0-35-16.3-35-36.5V279.1L266.8,449.2c-14.2,14.8-37.1,14.8-51.3,0l-51.3-53.4c-14.2-14.8-14.2-38.7,0-53.4L453.2,41.1c1.2-1.3,23.7-15.6,46.4-15.6c22.9,0,45.9,14.3,47.2,15.6l289.1,301.2c14.2,14.8,14.2,38.7,0,53.4L784.5,449.2L784.5,449.2z'
+                                    />
+                                </g>
+                            </Icon>
+                        </Text>
+                    )}
+
+                    {imageSrc && (
+                        <Image borderRadius={'5px'} my={'auto'} mx={'auto'} width={'50%'} maxWidth={'300px'}
+                               src={imageSrc}/>
+                    )}
+                </Box>
+
+                {imageSrc && !uploadData && (
+                    <Box shadow={'sm'} mt={'20px'} fontWeight={'700'} borderRadius={'10px'} justifyContent={'center'}
+                         backgroundColor={'white'} witdh={'80px'} mx={'auto'}>
+                        <button style={{
+                            height: '35px',
+                            margin: 'auto',
+                            textAlign: 'center',
+                            display: "block",
+                            fontFamily: 'monospace'
+                        }}>写真を共有する
+                        </button>
+                    </Box>
+                )}
+
+                {uploadData && (
+                    <code>
+                        <pre>{JSON.stringify(uploadData, null, 2)}</pre>
+                    </code>
+                )}
+            </form>
         </Box>
-
-        {imageSrc && !uploadData && (
-          <p>
-            <button>Upload Files</button>
-          </p>
-        )}
-
-        {uploadData && (
-          <code>
-            <pre>{JSON.stringify(uploadData, null, 2)}</pre>
-          </code>
-        )}
-      </form>
-    </Box>
-  );
+    );
 };
 
 export default Photos;
